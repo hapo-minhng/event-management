3b40239252a0dfbc878a072e1ee3626ea214150e
Minh Nguyen <minhng@haposoft.com>
2024-12-18 13:36:12 +0700

    Apply Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:sanctum')
     ->get('/user', function (Request $request) {
         return $request->user();
     });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
